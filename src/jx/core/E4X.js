a73810c5b5ae739dff75f6b41683bbf9f66fe046
Thomas DiG <thomas@devingfx.com>
2016-07-19 19:26:08 +0200

    On branch master  Changes to be committed: 	modified:   src/jx/core/E4X.js

--- src/jx/core/E4X.js
+++ src/jx/core/E4X.js
@@ -21,76 +21,222 @@ window.XMLList = class XMLList extends Array {
 	select( query )
 	{
 		var flat = [];
-		this.map( node => flat = flat.concat( Array.from(node.querySelectorAll(query)) )	);
+		this.map( node => flat = flat.concat(Array.from( node.querySelectorAll(query)) )	);
 		return new XMLList( flat );
 	}
 	
+	/**
+	 * attribute(attributeName:*):XMLList
+	 * Calls the attribute() method of each XML object and returns an XMLList object of the results.
+	 */
+	attribute( attributeName )
+	{
+		var flat = [];
+		this.map( node => flat = flat.concat(Array.from( node.attribute(attributeName)) )	);
+		return new XMLList( flat );
+	}
 	/*
-	attribute(attributeName:*):XMLList
-	Calls the attribute() method of each XML object and returns an XMLList object of the results.
-	 	 	
-	attributes():XMLList
-	Calls the attributes() method of each XML object and returns an XMLList object of attributes for each XML object.
-	 	 	
-	child(propertyName:Object):XMLList
-	Calls the child() method of each XML object and returns an XMLList object that contains the results in order.
-	 	 	
-	children():XMLList
-	Calls the children() method of each XML object and returns an XMLList object that contains the results.
-	 	 	
-	comments():XMLList
-	Calls the comments() method of each XML object and returns an XMLList of comments.
-	 	 	
-	contains(value:XML):Boolean
-	Checks whether the XMLList object contains an XML object that is equal to the given value parameter.
-	 	 	
-	copy():XMLList
-	Returns a copy of the given XMLList object.
-	 	 	
-	descendants(name:Object = *):XMLList
-	Returns all descendants (children, grandchildren, great-grandchildren, and so on) of the XML object that have the given name parameter.
-	 	 	
-	elements(name:Object = *):XMLList
-	Calls the elements() method of each XML object.
-	 	 	
-	hasComplexContent():Boolean
-	Checks whether the XMLList object contains complex content.
-	 	 	
-	hasOwnProperty(p:String):Boolean
-	Checks for the property specified by p.
-	 	 	
-	hasSimpleContent():Boolean
-	Checks whether the XMLList object contains simple content.
-	 	 	
-	length():int
-	Returns the number of properties in the XMLList object.
-	 	 	
-	normalize():XMLList
-	Merges adjacent text nodes and eliminates empty text nodes for each of the following: all text nodes in the XMLList, all the XML objects contained in the XMLList, and the descendants of all the XML objects in the XMLList.
-	 	 	
-	parent():Object
-	Returns the parent of the XMLList object if all items in the XMLList object have the same parent.
-	 	 	
-	processingInstructions(name:String = "*"):XMLList
-	If a name parameter is provided, lists all the children of the XMLList object that contain processing instructions with that name.
-	 	 	
-	propertyIsEnumerable(p:String):Boolean
-	Checks whether the property p is in the set of properties that can be iterated in a for..in statement applied to the XMLList object.
-	 	 	
-	text():XMLList
-	Calls the text() method of each XML object and returns an XMLList object that contains the results.
-	 	 	
-	toString():String
-	Returns a string representation of all the XML objects in an XMLList object.
-	 	 	
-	toXMLString():String
-	Returns a string representation of all the XML objects in an XMLList object.
-	 	 	
-	valueOf():XMLList
-	Returns the XMLList object.
-
-	*/
+	 * attributes():XMLList
+	 * Calls the attributes() method of each XML object and returns an XMLList object of attributes for each XML object.
+	 */
+	attributes()
+	{
+		var flat = [];
+		this.map( node => flat = flat.concat( Array.from(node.attributes()) )	);
+		return new XMLList( flat );
+	}
+	/**
+	 * child(propertyName:Object):XMLList
+	 * Calls the child() method of each XML object and returns an XMLList object that contains the results in order.
+	 */
+	child( propertyName )
+	{
+		var flat = [];
+		this.map( node => flat = flat.concat(Array.from( node.child(propertyName)) )	);
+		return new XMLList( flat );
+	}
+	/**
+	 * children():XMLList
+	 * Calls the children() method of each XML object and returns an XMLList object that contains the results.
+	 */
+	children( propertyName )
+	{
+		var flat = [];
+		this.map( node => flat = flat.concat(Array.from( node.children(propertyName)) )	);
+		return new XMLList( flat );
+	}
+	/**
+	 * comments():XMLList
+	 * Calls the comments() method of each XML object and returns an XMLList of comments.
+	 */
+	comments()
+	{
+		var flat = [];
+		this.map( node => flat = flat.concat(Array.from( node.comments()) )	);
+		return new XMLList( flat );
+	}
+	/**
+	 * contains(value:XML):Boolean
+	 * Checks whether the XMLList object contains an XML object that is equal to the given value parameter.
+	 */
+	contains( value )
+	{
+		return this.filter( node => node === value ).length > 0;
+	}
+	/**
+	 * copy():XMLList
+	 * Returns a copy of the given XMLList object.
+	 */
+	copy()
+	{
+		return new XMLList( this );
+	}
+	/**
+	 * descendants(name:Object = *):XMLList
+	 * Returns all descendants (children, grandchildren, great-grandchildren, and so on) of the XML object that have the given name parameter.
+	 */
+	descendants( name )
+	{
+		
+	}
+	/**
+	 * elements(name:Object = *):XMLList
+	 * Calls the elements() method of each XML object.
+	 */
+	elements( name )
+	{
+		var flat = [];
+		this.map( node => flat = flat.concat(Array.from( node.elements(name)) )	);
+		return new XMLList( flat );
+	}
+	/**
+	 * hasComplexContent():Boolean
+	 * Checks whether the XMLList object contains complex content.
+	 */
+	hasComplexContent()
+	{
+		
+	}
+	/**
+	 * hasOwnProperty(p:String):Boolean
+	 * Checks for the property specified by p.
+	 */
+	hasOwnProperty( p )
+	{
+		
+	}
+	/**
+	 * hasSimpleContent():Boolean
+	 * Checks whether the XMLList object contains simple content.
+	 */
+	hasSimpleContent()
+	{
+		
+	}
+	/**
+	 * length():int
+	 * Returns the number of properties in the XMLList object.
+	 */
+	length()
+	{
+		
+	}
+	/**
+	 * normalize():XMLList
+	 * Merges adjacent text nodes and eliminates empty text nodes for each of the following: all text nodes in the XMLList, all the XML objects contained in the XMLList, and the descendants of all the XML objects in the XMLList.
+	 */
+	normalize()
+	{
+		
+	}
+	/**
+	 * parent():Object
+	 * Returns the parent of the XMLList object if all items in the XMLList object have the same parent.
+	 */
+	parent()
+	{
+		return this.map( node => node.parent() )
+					.every( parent => parent === this[0].parent() ) ?
+						
+						this[0].parent()
+						:
+						null
+	}
+	/**
+	 * processingInstructions(name:String = "*"):XMLList
+	 * If a name parameter is provided, lists all the children of the XMLList object that contain processing instructions with that name.
+	 */
+	processingInstructions( name )
+	{
+		
+	}
+	/**
+	 * propertyIsEnumerable(p:String):Boolean
+	 * Checks whether the property p is in the set of properties that can be iterated in a for..in statement applied to the XMLList object.
+	 */
+	propertyIsEnumerable( p )
+	{
+		
+	}
+	/**
+	 * text():XMLList
+	 * Calls the text() method of each XML object and returns an XMLList object that contains the results.
+	 */
+	text()
+	{
+		var flat = [];
+		this.map( node => flat = flat.concat(Array.from( node.text()) )	);
+		return new XMLList( flat );
+	}
+	/**
+	 * toString():String
+	 * Returns a string representation of all the XML objects in an XMLList object.
+	 */
+	toString()
+	{
+		
+	}
+	/**
+	 * toXMLString():String
+	 * Returns a string representation of all the XML objects in an XMLList object.
+	 */
+	toXMLString()
+	{
+		
+	}
 }
+
+
+window.__CallableArray_symbol = Symbol('Callable');
+function CallableArray( ...args )
+{
+	var length = 0,
+		fn = function()
+		{
+			return fn[CallableArray.symbol].apply( fn, arguments )
+		};
+	
+	Object.setPrototypeOf( fn, Array.prototype );
+	Object.defineProperty( fn, 'length', {get: () => length, set: v => length = v} )
+	// fn.flat = function( ...args )
+	// {
+	// 	return fn;
+	// }
+	// fn.flat( args );
+	args.map( arg => Array.from(arg).map(o => fn.push(o)) )
+	return fn;
+}
+CallableArray.symbol = __CallableArray_symbol;
+delete window.__CallableArray_symbol;
+
+
+
+var af = new CallableArray([]);
+af[CallableArray.symbol] = function( s )
+{
+	this.map( item => this.concat(item.querySelectorAll(s)) )
+};
+af()
 
 class E4X {
 	
@@ -111,7 +257,7 @@ class E4X {
 				
 			};
 		
-		Object.defineProperty( X, 'length', { get: () => length, set: v => length = v})
+		Object.defineProperty( X, 'length', {get: () => length, set: v => length = v} )
 		Object.setPrototypeOf( X, Array.prototype );
 		
 		childs = Array.from( node.children, child => {
@@ -130,16 +276,21 @@ class E4X {
 		return X;
 	}
 	
+	static mapFlat()
+	{
+		var flat = [];
+		this.map( node => flat = flat.concat(Array.from( node.querySelectorAll(query)) )	);
+		return new XMLList( flat );
+	}
 }
 
 
-class XML extends E4X {
-	
-	constructor()
+function XML( node )
+{
+	if( node instanceof HTMLDocument )
 	{
 		
 	}
-	
 }
 
 // def( window, document.documentElement.localName, {
@@ -148,7 +299,7 @@ class XML extends E4X {
 // 		return new E4X( document.documentElement );
 // 	}
 // })
-window.html = (new E4X( document )).html;
+window.html = XML( document ).html;
 
 /* SPECS * /
 
